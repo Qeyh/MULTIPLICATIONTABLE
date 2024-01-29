80bff04882f134bc1cdc83011057ffd1fd9709a7
Qeyh <daryakerman2002@gmail.com>
2024-01-29 18:54:57 +0200

    commiting

---
+++ main.py
@@ -0,0 +1,23 @@
+multi_table = []
+
+maxTable=11
+
+#create table
+for i in range(maxTable):
+    row = []
+    for j in range(maxTable):
+        row.append(i * j)
+    multi_table.append(row)
+
+#delete first row of only 0's to make it better looking
+del multi_table[0]
+
+#good looking print
+print("############################################")
+print("MULTIPLICATION TABLE by Dary, Sela H.W ")
+for row in multi_table:
+    for i in row:
+        print(i, end=" ")
+    print("\n")
+
+print("############################################")
